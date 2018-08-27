95a6accd15121305c9362318cd36199c359bf3b5
kruchkamgar <kruchkamgar@gmail.com>
2018-08-27 17:57:53 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,11 @@
 def count_elements(array)
-  # code goes here
+  animal_types = array.uniq
+  animal_count = {}
+
+  array.each do |animal|
+  	animal_count[animal] = array.count(animal)
+  end
+
+animal_count
 end
  
